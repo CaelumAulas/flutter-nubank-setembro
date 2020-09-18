e41d46a190b43b7e9916e1abc2cbcb93980c02f0
Gabriel Savio <gabulsavul@outlook.com>
2020-09-18 10:13:23 -0300

    desafio 1

--- aula5/lib/app/login/login_page.dart
+++ aula5/lib/app/login/login_page.dart
@@ -2,6 +2,18 @@ import 'package:aula5/app/home/home_page.dart';
 import 'package:aula5/main.dart';
 import 'package:flutter/material.dart';
 
+//TODO
+//Desafio1
+//-Criar as validacoes do form
+//-Armazenar o email e senha em uma variavel ou objeto
+//- Validar se o email e senha digitados sao autorizados
+
+//email= email@email.com
+//senha= 123456
+
+///Desafio 2
+//Nao autorizado exibir uma mensagem de error?
+
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
 
